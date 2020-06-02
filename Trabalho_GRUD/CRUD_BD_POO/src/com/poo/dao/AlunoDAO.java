da023296d2e4c474358972aedc0f89ddfefa2225
Alessandro Fidelis <alessandro.fidelis@hotmail.com>
2020-06-02 20:42:18 -0300

    Update AlunoDAO.java

--- Trabalho_GRUD/CRUD_BD_POO/src/com/poo/dao/AlunoDAO.java
+++ Trabalho_GRUD/CRUD_BD_POO/src/com/poo/dao/AlunoDAO.java
@@ -1,7 +1,6 @@
 package com.poo.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -70,52 +69,38 @@ public class AlunoDAO {
 	public void update(Aluno aluno) {
 
 		try {
-			// create the mysql database connection
-			String Driver = "com.mysql.jdbc.Driver";
-			String Url = "jdbc:mysql://localhost:3306/aluno";
-			Class.forName(Driver);
-			Connection conn = DriverManager.getConnection(Url, "root", "");
+			
+			Connection con = ConnectionFactory.getConnection();
+			PreparedStatement stmt = null;
 
-			// create the mysql delete statement.
-			// i'm deleting the row where the id is "3", which corresponds to my
-			// "Barney Rubble" record.
+			
 			String query = "UPDATE aluno SET nome=? WHERE matricula=?";
-			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			PreparedStatement preparedStmt = con.prepareStatement(query);
 			preparedStmt.setString(1, aluno.getNome());
 			preparedStmt.setString(2, aluno.getMatricula());
-
-			// execute the preparedstatement
 			preparedStmt.execute();
 
-			conn.close();
+			con.close();
 		} catch (Exception e) {
-			System.err.println("Got an exception! ");
-			System.err.println(e.getMessage());
 		}
 	}
 
 	public void delete(Aluno aluno) {
 		try {
-			// create the mysql database connection
-			String Driver = "com.mysql.jdbc.Driver";
-			String Url = "jdbc:mysql://localhost:3306/aluno";
-			Class.forName(Driver);
-			Connection conn = DriverManager.getConnection(Url, "root", "");
+			
+			Connection con = ConnectionFactory.getConnection();
+			PreparedStatement stmt = null;
 
-			// create the mysql delete statement.
-			// i'm deleting the row where the id is "3", which corresponds to my
-			// "Barney Rubble" record.
+			
 			String query = "delete from aluno where matricula = ?";
-			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			PreparedStatement preparedStmt = con.prepareStatement(query);
 			preparedStmt.setString(1, aluno.getMatricula());
 
-			// execute the preparedstatement
+			
 			preparedStmt.execute();
 
-			conn.close();
+			con.close();
 		} catch (Exception e) {
-			System.err.println("Got an exception! ");
-			System.err.println(e.getMessage());
 		}
 	}
 }
